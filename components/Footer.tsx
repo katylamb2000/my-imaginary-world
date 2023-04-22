2b73e714daecb3a95e42905c4a29eb6d157bd992
Katy Lamb <katylamb@Katys-MacBook-Pro.local>
2023-04-22 15:19:56 +0100

    footer 2

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='w-screen h-14 bg-white bottom-0 text-center'>
+    <div className='w-screen h-14 bg-white bottom-0 text-center justify-evenly'>
+      <p className='cursor-pointer text-purple-600 text-sm'>Jobs</p>
       <p className='cursor-pointer text-purple-600 font-bold'>My Imaginary World</p>
+      <p className='cursor-pointer text-purple-600 text-sm'>Contact us</p>
+
     </div>
   )
 }
